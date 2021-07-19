540be69b05435dda65de23fa7a26dc567373bcfe
yymin1022 <yymin1022@gmail.com>
2021-07-19 13:35:17 +0000

    Update message.py : Add Message Function Ah

--- message.py
+++ message.py
@@ -6,7 +6,21 @@ def getReplyMessage(message):
     return strResult
 
 def messageAh():
-    pass
+    randInt = random.randrange(0, 5)
+    strMessage = ""
+
+    if randInt == 0:
+        strMessage = "글쿤.."
+    elif randInt == 1:
+        strMessage = "그래요.."
+    elif randInt == 2:
+        strMessage = "그렇군요.."
+    elif randInt == 3:
+        strMessage = "안돼.."
+    elif randInt == 4:
+        strMessage = "메리카노.."
+    
+    return strMessage
 
 def messageCoding():
     pass
@@ -30,7 +44,19 @@ def messageStop():
     pass
 
 def messageWa():
-    pass
+    randInt = random.randrange(0, 5)
+    messageStr = ""
+
+    if randInt == 0:
+        messageStr = "갑부;;"
+    elif randInt == 1:
+        messageStr = "기만;;"
+    elif randInt == 2:
+        messageStr = "ㄹㅇ;;"
+    elif randInt == 3:
+        messageStr = "마스터;;"
+    elif randInt == 4:
+        messageStr = "역시;;"
 
 def messageWaSans():
     pass

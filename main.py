09b0ed6ebc125a87bddec56b596cf6182f9a2bf6
yymin1022 <yymin1022@gmail.com>
2021-07-19 11:41:42 +0000

    Update main.py : Add Message Controller with Controller Class

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
 
+import message
+
 flaskApp = Flask (__name__)
  
 @flaskApp.route("/")
@@ -9,7 +11,18 @@ def main():
 @flaskApp.route("/getMessage", methods = ["POST"])
 def getMessage():
     inputData = request.get_json()
-    return jsonify(inputData)
+
+    inputMessage = inputData["msg"]
+    inputRoom = inputData["room"]
+    inputSender = inputData["sender"]
+
+    replyMessage = message.getReplyMessage(inputMessage)
+    replyRoom = inputRoom
+    replySender = inputSender
+
+    replyData = dict(zip(("msg", replyMessage), ("room", replyRoom), ("sender", replySender)))
+
+    return jsonify(replyData)
  
 if __name__ == "__main__":
     flaskApp.run(host = "0.0.0.0", port = 80)

933ff12fafbc5eabc9c4da7f49d787d83eed09bf
yymin1022 <yymin1022@gmail.com>
2021-07-19 10:39:38 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 flaskApp = Flask (__name__)
  
-@flaskApp.route('/')
+@flaskApp.route("/")
 def main():
-    return 'Hello, World!'
+    return "Hello, World!"
  
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 80)

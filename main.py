d7b0c12dc5fa020ed80775a30abca170ead6c819
yymin1022 <yymin1022@gmail.com>
2021-07-19 10:14:27 +0000

    Update main.py : Add Falsk Basic Code

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+from flask import Flask
+
+flaskApp = Flask (__name__)
+ 
+@flaskApp.route('/')
+def main():
+    return 'Hello, World!'
+ 
+if __name__ == "__main__":
+    app.run()

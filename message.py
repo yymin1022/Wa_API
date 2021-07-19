b05b28250532d1ce74c4de79970ef49c25369210
yymin1022 <yymin1022@gmail.com>
2021-07-19 13:33:54 +0000

    Update message.py : Add Message Functions

--- message.py
+++ message.py
@@ -4,3 +4,39 @@ def getReplyMessage(message):
     strResult = message
 
     return strResult
+
+def messageAh():
+    pass
+
+def messageCoding():
+    pass
+
+def messageHa():
+    pass
+
+def messageIreon():
+    pass
+
+def messageMooYaHo():
+    pass
+
+def messageReal():
+    pass
+
+def messageSalute():
+    pass
+
+def messageStop():
+    pass
+
+def messageWa():
+    pass
+
+def messageWaSans():
+    pass
+
+def messageZara():
+    pass
+
+def messageZayazi():
+    pass

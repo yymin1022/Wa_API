1109b28bde3d9bb737089ae7906cec35e15f743c
yymin1022 <yymin1022@gmail.com>
2021-07-24 08:19:55 +0000

    Update setupServer.sh

--- setupServer.sh
+++ setupServer.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-echo "Installing Requiered Packages"
+echo "Installing Requiered Packages..."
+echo ""
 sudo apt install apache2 libapache2-mod-wsgi-py3 python3 python3-flask python3-pip -y
 
+echo "Enabling Apache2 WSGI Module..."
+sudo a2enmod wsgi
+echo ""
+
+echo "Installing Python3 Required Modules with PIP"
+python3 -m pip install flask requests
+echo ""
+
+echo
 echo "Please input Port Number for WA API Server"
 read serverPort
+echo ""
 
 if [ $serverPort = "80" ]
 then
@@ -18,16 +29,20 @@ curDirectory=$(pwd)
 
 sudo sed -i "s|varDirectory|$curDirectory|g" /etc/apache2/sites-available/000-default.conf
 sudo sed -i "s|varPort|$serverPort|g" /etc/apache2/sites-available/000-default.conf
+echo ""
 
 echo "Setup Done..!"
+echo ""
 
 echo "Start API Server from now? (y/n)"
 
 read doStart
+echo ""
 
 if [ $doStart = "y" ]
 then
     sudo /etc/init.d/apache2 start
 else
     echo "You can start API Server with this command : /etc/init.d/apache2 start"
+    echo ""
 fi

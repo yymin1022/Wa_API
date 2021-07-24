ffdfa6beda14d279f7546014626f4d4ffaf49f2a
hiru <rlawoehd187@naver.com>
2021-07-24 15:30:57 +0900

    Update message.py : Add more message in hokyu

--- message.py
+++ message.py
@@ -280,11 +280,13 @@ def messageHokyu():
     
     randInt = random.randrange(0, 3)
     if randInt == 0:
-    	strMessage = "호규는 2021년 7월 19일 입대했습니다. 2023년 1월 18일 전역 예정입니다. %d일 남았습니다."%(leftDays)
+	strMessage = "호규는 2021년 7월 19일 입대했습니다. 2023년 1월 18일 전역 예정입니다. %d일 남았습니다."%(leftDays)
     elif randInt == 1:
-    	strMessage = "호규가 입대한 지 %d일 되었습니다."%(goneDays)
+	strMessage = "호규가 입대한 지 %d일 되었습니다."%(goneDays)
     elif randInt == 2:
-	    strMessage = "안녕하세요? 아미타이거 육군 김호규입니다."
+	strMessage = "안녕하세요? 아미타이거 육군 김호규입니다."
+    elif randInt == 3:
+	strMessage = "응애 나 애기 호규."
     
     return strMessage
 

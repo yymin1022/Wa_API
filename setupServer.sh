5fde766de34b91355a73cb97aa275e33cc4af393
yymin1022 <yymin1022@gmail.com>
2021-07-24 07:22:48 +0000

    Update setupServer.sh : Add Basic Commandlines

---
+++ setupServer.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Installing Requiered Packages"
+sudo apt install apache2 libapache2-mod-wsgi-py3 python3 python3-flask python3-pip -y
+
+while:
+do
+    echo "Please input Port Number for WA API Server"
+    read serverPort
+done
+
+if [ $serverPort = "80" ]
+then
+    sudo cat apache2script > /etc/apache2/sites-available/000-default.conf
+else
+    sudo cat apache2script >> /etc/apache2/sites-available/000-default.conf
+    sudo echo Listen $serverPort >> /etc/apache2/ports.conf
+fi
+
+echo "Setup Done..!"
+
+echo "Start API Server from now? (y/n)"
+
+read doStart
+
+if [$doStart = "y"]
+then
+    sudo /etc/init.d/apache2 start
+else
+    echo "You can start API Server with this command : /etc/init.d/apache2 start"
+fi

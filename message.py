05a111e45ca56dc5051a1840d74a7a74e94543cd
yymin1022 <yymin1022@gmail.com>
2021-07-29 05:04:41 +0000

    Update message.py : Improve Message Function, Shuk

--- message.py
+++ message.py
@@ -266,21 +266,21 @@ def messageSaseyo():
 
 def messageShuk():
     randInt = random.randrange(0, 4)
-    strMessage = ""
+    strMessage = "슈슉"
     
     while randInt != 4:
         if randInt == 0:
-            strMessage += "슉.슈슉.시.발럼."
+            strMessage += ".슉.슈슉.시.발럼"
         elif randInt == 1:
-            strMessage += "슈슉.슉.슉시."
+            strMessage += ".슈슉.슉.슉시."
         elif randInt == 2:
-            strMessage += "슈발놈아.슉.시발.슈슉.슉."
+            strMessage += ".슈발놈아.슉.시발.슈슉.슉."
         elif randInt == 3:
-            strMessage += "슈슉.시발.럼아.슉.슈슉.슉.슉슉.슈슉.시.발놈아."
+            strMessage += ".슈슉.시발.럼아.슉.슈슉.슉.슉슉.슈슉.시.발놈아."
         
         randInt = random.randrange(0, 5)
 
-    strMessage += ".슈슉"
+    strMessage += ".슉"
 
     return strMessage
 

e0261cf403eb8a54747a48bc4cb81417cfe77a38
yymin1022 <yymin1022@gmail.com>
2021-10-07 21:06:57 +0900

    Revert "Update message.py : Cleand UP"
    This reverts commit 250131f06d60322b1d5c291eccf5fc14a956c1a9.

--- message.py
+++ message.py
@@ -71,7 +71,7 @@ def getReplyMessage(message):
         strResult = messageYongminGraduate()
     elif "용민" in message:
         strResult = messageYongmin()
-    elif "자라" in message  or "zara" in message.lower():
+    elif "자라" in message:
         strResult = messageZara()
     elif "자야" in message:
         strResult = messageZayazi()

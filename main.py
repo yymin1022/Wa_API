ce02b7b0222c13c834c6bff5203a9cf0cc95fab1
yymin1022 <yymin1022@gmail.com>
2021-12-22 14:15:08 +0900

    Cleaned UP

--- main.py
+++ main.py
@@ -1,48 +1,10 @@
 from flask import Flask, jsonify, redirect, request
 
-import message
-
 flaskApp = Flask(__name__)
  
 @flaskApp.route("/")
 def main():
-    return redirect("https://github.com/yymin1022/Wa_API", code=302)
+    return "Hello, CECOM!"
 
-@flaskApp.route("/getMessage", methods = ["POST"])
-def getMessage():
-    errCode = 0
-    errMessage = "RESULT OK"
-
-    inputData = ""
-    inputMessage = ""
-    inputRoom = ""
-    inputSender = ""
-
-    try:
-        inputData = request.get_json()
-
-        inputMessage = inputData["msg"]
-        inputRoom = inputData["room"]
-        inputSender = inputData["sender"]
-    except Exception as errContent:
-        errCode = 200
-        errMessage = repr(errContent)
-
-        replyData = dict([("RESULT", dict([("RESULT_CODE", errCode), ("RESULT_MSG", errMessage)])), ("DATA", dict([("msg", ""), ("room", ""), ("sender", "")]))])
-
-        return jsonify(replyData)
-
-    replyMessage = message.getReplyMessage(inputMessage)
-    replyRoom = inputRoom
-    replySender = inputSender
-
-    if(replyMessage == ""):
-        errCode = 100
-        errMessage = "None WA Bot Message Found"
-
-    replyData = dict([("RESULT", dict([("RESULT_CODE", errCode), ("RESULT_MSG", errMessage)])), ("DATA", dict([("msg", replyMessage), ("room", replyRoom), ("sender", replySender)]))])
-
-    return jsonify(replyData)
- 
 if __name__ == "__main__":
     flaskApp.run(host="0.0.0.0", port=80)

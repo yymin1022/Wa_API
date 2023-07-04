158a689cc775660fd1e57de54720322151b49081
Changhwan Kim <kimch061279@gmail.com>
2023-07-04 17:55:37 +0900

    messageRemember 함수와 messageRemreturn 함수의 위치를 알파벳의 위치에 맞게 이동

--- message.py
+++ message.py
@@ -778,6 +778,39 @@ def messageReal():
 
     return strMessage
 
+def messageRemember(message, room):
+    if os.path.isfile("rem.json"):
+        with open('rem.json', 'r', encoding='utf-8') as f:
+            rem_dict = json.load(f)
+    else:
+        rem_dict = {}
+
+    message = message.replace("!기억", "")
+    rem_dict[room] = message
+    json_data = json.dumps(rem_dict, ensure_ascii=False, indent=4)
+
+    with open('rem.json', 'w', encoding='utf-8') as f:
+        f.write(json_data)
+
+    strMessage = ""
+    return strMessage
+
+def messageRemreturn(room):
+    strMessage = ""
+
+    if os.path.isfile("rem.json"):
+        with open('rem.json', 'r', encoding='utf-8') as f:
+            rem_dict = json.load(f)
+
+        if room in rem_dict:
+            strMessage = rem_dict[room] + "\\m아마 이거일 듯?"
+        else:
+            strMessage = ""
+    else:
+        strMessage = ""
+
+    return strMessage
+
 def messageSalute():
     randInt = random.randrange(0, 2)
     strMessage = ""
@@ -988,37 +1021,4 @@ def messageWeather():
     jsonData = json.loads(text)
     
     strMessage = "현재온도: " + str((jsonData["main"]["temp"])) + "구름: " + str((jsonData["clouds"]["all"]))
-    return strMessage
-
-def messageRemember(message, room):
-    if os.path.isfile("rem.json"):
-        with open('rem.json', 'r', encoding='utf-8') as f:
-            rem_dict = json.load(f)
-    else:
-        rem_dict = {}
-
-    message = message.replace("!기억", "")
-    rem_dict[room] = message
-    json_data = json.dumps(rem_dict, ensure_ascii=False, indent=4)
-
-    with open('rem.json', 'w', encoding='utf-8') as f:
-        f.write(json_data)
-
-    strMessage = ""
-    return strMessage
-
-def messageRemreturn(room):
-    strMessage = ""
-
-    if os.path.isfile("rem.json"):
-        with open('rem.json', 'r', encoding='utf-8') as f:
-            rem_dict = json.load(f)
-
-        if room in rem_dict:
-            strMessage = rem_dict[room] + "\\m아마 이거일 듯?"
-        else:
-            strMessage = ""
-    else:
-        strMessage = ""
-
     return strMessage

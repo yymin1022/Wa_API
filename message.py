d32043500be357b09a10a8880c1955318c98d4fa
Changhwan Kim <kimch061279@gmail.com>
2023-09-13 10:47:08 +0900

    병희전역 기능 추가

--- message.py
+++ message.py
@@ -318,8 +318,11 @@ def messageBuy():
     return strMessage
 
 def messageBHGraduate():
-    strMessage = "임병희씨가 입대한지 %d일, 전역한지는 %d일이 됐습니다."%((datetime.date.today() - datetime.date(2020,6,30)).days, (datetime.date.today() - datetime.date(2021,12,29)).days)
+    strMessage = ""
 
+    i = random.randint(0,2)
+    if i == 0: strMessage = "임병희씨가 입대한지 %d일, 전역한지는 %d일이 됐습니다."%((datetime.date.today() - datetime.date(2020,6,30)).days, (datetime.date.today() - datetime.date(2021,12,29)).days)
+    elif i == 1: strMessage = "임병희씨의 예비군 소집해제일까지 %d일 남았습니다."%((datetime.date.today() - datetime.date(2029,12,31)).days)
     return strMessage
 
 def messageCAUCalendar():

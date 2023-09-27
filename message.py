6ecf369ab802d4329e49e226441a67885bfd79e4
Changhwan Kim <kimch061279@gmail.com>
2023-09-27 10:14:38 +0900

    edit CustomTracker Function; fix err

--- message.py
+++ message.py
@@ -482,8 +482,6 @@ def messageCustomTracker(message):
     strMessage = ""
     try:
         message = message.replace('!통관 ', '')
-        if message.isdigit() == False:
-            raise
         key = os.environ['CUSTOM_API_KEY']
         year = datetime.date.today().year
         url = 'https://unipass.customs.go.kr:38010/ext/rest/cargCsclPrgsInfoQry/retrieveCargCsclPrgsInfo?crkyCn=%s&blYy=%s&hblNo=%s' % (key, year, message)
@@ -492,7 +490,7 @@ def messageCustomTracker(message):
         name = soup.find('prnm')
         status = soup.find('csclPrgsStts')
         process_time = datetime.datetime.strptime(str(soup.find('prcsDttm').text), "%Y%m%d%H%M%S").strftime("%Y.%m.%d %H:%M:%S")
-        strMessage = "/// 국세청 UNIPASS 통관 조회 ///\n\n품명: %s\n통관진행상태: %s\n처리일시: %s" % (name, status, process_time)
+        strMessage = "/// 국세청 UNIPASS 통관 조회 ///\n\n품명: %s\n통관진행상태: %s\n처리일시: %s" % (name.text, status.text, process_time)
     except:
         strMessage = "존재하지 않는 운송장번호이거나 잘못된 형식 혹은 아직 입항하지 않은 화물입니다.\\m사용법: !통관 123456789"
     

a4f94c10872e0d25d1268ace98a09f415c0cf459
Changhwan Kim <kimch061279@gmail.com>
2023-11-27 14:42:05 +0900

    edit room, sender line

--- apitest(direct).py
+++ apitest(direct).py
@@ -2,12 +2,8 @@ import message
 import os
 import json
 
-# Room과 Sender를 매번 입력받으려면 input()으로 문장을 고치세요.
-inputRoom = "Test_Room1"
-inputSender = "Test_User1"
-
 inputMessage = input("테스트 할 Message를 입력하세요: ")
-replyMessage = message.getReplyMessage(inputMessage, inputRoom, inputSender)
+replyMessage = message.getReplyMessage(inputMessage, "Test_Room1", "Test_Sender1")
 
 if replyMessage == "":
     replyMessage = "None"

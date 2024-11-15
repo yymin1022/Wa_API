c0261431be3029010ff50a96ac3aa7d18f303afe
yymin1022 <yymin1022@gmail.com>
2024-11-15 13:25:38 +0900

    Update message.py : Adjust Gemini Config

--- message.py
+++ message.py
@@ -24,42 +24,12 @@ CIPHERS = (
 
 load_dotenv()
 GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY')
-GEMINI_GENERATION_CONFIG = {
-  "candidate_count": 1,
-  "max_output_tokens": 256,
-  "temperature": 1.0,
-  "top_p": 0.7,
-}
-GEMINI_SAFETY_CONFIG=[
-  {
-    "category": "HARM_CATEGORY_DANGEROUS",
-    "threshold": "BLOCK_NONE",
-  },
-  {
-    "category": "HARM_CATEGORY_HARASSMENT",
-    "threshold": "BLOCK_NONE",
-  },
-  {
-    "category": "HARM_CATEGORY_HATE_SPEECH",
-    "threshold": "BLOCK_NONE",
-  },
-  {
-    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-    "threshold": "BLOCK_NONE",
-  },
-  {
-    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-    "threshold": "BLOCK_NONE",
-  },
-]
 
 genai.configure(
     api_key=GEMINI_API_KEY
 )
 model = genai.GenerativeModel(
     model_name='gemini-1.5-flash-8b-latest',
-    generation_config=GEMINI_GENERATION_CONFIG,
-    safety_settings=GEMINI_SAFETY_CONFIG,
     system_instruction="당신은 대한민국의 초등학생입니다. 한국어 반말을 이용해서 대답을 해주면 됩니다. 하지만 답변의 진실 여부는 절대로 틀리면 안됩니다. 무조건 옳은 답변을 지정된 말투로 말해주세요."
 )
 

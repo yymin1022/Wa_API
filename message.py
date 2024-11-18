3488e66e5cce8565cf253fa99e8c39ce1d34c6c3
yymin1022 <yymin1022@gmail.com>
2024-11-19 03:24:36 +0900

    Update message_graduate.py : Migrated Message Functions

--- message.py
+++ message.py
@@ -15,11 +15,15 @@ def getReplyMessage(message, room, sender):
     if result_message is not None:
         return result_message
 
+    result_message = message.message_logistics(message, room, sender)
+    if result_message is not None:
+        return result_message
+
     result_message = message.message_cry_laugh_stress(message, room, sender)
     if result_message is not None:
         return result_message
 
-    result_message = message.message_logistics(message, room, sender)
+    result_message = message.message_graduate(message, room, sender)
     if result_message is not None:
         return result_message
 
@@ -47,33 +51,13 @@ def getReplyMessage(message, room, sender):
         else:
             strResult = messageCAULibrary("")
     elif "호규" in message:
-        if "전역" in message:
-            strResult = messageHokyuGraduate()
-        else:
-            strResult = messageHokyu()
+        strResult = messageHokyu()
     elif "주형" in message:
         strResult = messageJoohyeong()
     elif "민식" in message:
         strResult = messageMinsik()
     elif "민석" in message:
         strResult = messageMinseok()
-    elif "소해" in message or "졸업" in message or "전역" in message:
-        if "승범" in message:
-            strResult = messageSeungbeomGraduate()
-        elif "성민" in message:
-            strResult = messageSeongminGraduate()
-        elif "수필" in message:
-            strResult = messageSupilGraduate()
-        elif "재민" in message:
-            strResult = messageJaeminGraduate()
-        elif "한수" in message:
-            strResult = messageHansuGraduate()
-        elif "병희" in message:
-            strResult = messageBHGraduate()
-        elif "창환" in message:
-            strResult = messageChalsGraduate()
-        elif "태식" in message:
-            strResult = messageTjoGraduate()
     elif "서건1우" in message:
         strResult = messageSGW()
     elif "유용민" in message:
@@ -122,14 +106,6 @@ def getReplyMessage(message, room, sender):
     elif "GDSC" in message:
         strResult = messageNotGDSC()
     return strResult
-
-def messageBHGraduate():
-    strMessage = ""
-
-    randInt = random.randrange(0,2)
-    if randInt == 0: strMessage = "임병희씨가 입대한지 %d일, 전역한지는 %d일이 됐습니다."%((datetime.date.today() - datetime.date(2020,6,30)).days, (datetime.date.today() - datetime.date(2021,12,29)).days)
-    elif randInt == 1: strMessage = "임병희씨의 예비군 소집해제일까지 %d일 남았습니다."%((datetime.date(2029,12,31) - datetime.date.today()).days)
-    return strMessage
 
 def messageBitcoin():
     strMessage = ""
@@ -206,11 +182,6 @@ def messageCAULibrary(libTypeID):
 
     return strMessage
 
-def messageChalsGraduate():
-    strMessage = "찰스가 입대한지 %d일, 전역한지는 %d일이 됐습니다."%((datetime.date.today() - datetime.date(2020,12,7)).days, (datetime.date.today() - datetime.date(2022,9,1)).days)
-
-    return strMessage
-
 def messageGemini(str):
     str = str.replace("잼민아", "").strip()
     response = gemini_model.generate_content(str)
@@ -233,48 +204,10 @@ def messageHokyu():
     ]
     return random.choice(messages)
 
-def messageHokyuGraduate():
-    strMessage = ""
-    randInt = random.randrange(0, 6)
-
-    if randInt == 0:
-        strMessage = "호규의 부사후 249기 지원을 응원합니다!"
-    elif randInt == 1:
-        strMessage = "호규의 학사 152기 지원을 응원합니다!"
-    elif randInt == 2:
-        strMessage = "호규의 예비군 소집해제일까지 %d일 남았습니다."%((datetime.date(2030,12,31) - datetime.date.today()).days -1)
-    elif randInt == 3:
-        strMessage = "호규의 민방위 소집해제일까지 %d일 남았습니다."%((datetime.date(2041,4,28) - datetime.date.today()).days -1)
-    elif randInt == 4:
-        strMessage = "예비군 1년차는 좀..."
-    elif randInt == 5:
-        strMessage = "하사 김호규의 임기제부사관 만기복무일까지 %d일 남았습니다."%((datetime.date(2027,8,26) - datetime.date.today()).days -1)
-
-    return strMessage
-
-def messageHansuGraduate():
-    strMessage = ""
-    randInt = random.randrange(0,2)
-    if randInt == 0: strMessage = "이한수씨가 소집된지 %d일, 해제된지는 %d일이 됐습니다."%((datetime.date.today() - datetime.date(2022,12,1)).days, (datetime.date.today() - datetime.date(2024,8,31)).days)
-    elif randInt == 1: strMessage = "이한수씨의 민방위 소집해제일까지 %d일 남았습니다."%((datetime.date(2039,1,1) - datetime.date.today()).days)
-
-    return strMessage
-
-def messageJaeminGraduate():
-    randInt = random.randrange(0, 2)
-    strMessage = ""
-
-    if randInt == 0: strMessage = "재민이가 입대한지 %d일, 전역한지는 %d일이 됐습니다."%((datetime.date.today() - datetime.date(2021,5,9)).days, (datetime.date.today() - datetime.date(2024,3,8)).days)
-    elif randInt == 1: strMessage = "재민이의 예비군 소집해제일까지 %d일 남았습니다."%((datetime.date(2031,12,31) - datetime.date.today()).days)
-
-    return strMessage
-
 def messageJoohyeong():
     strMessage = "예! 2025년도 CECOM 회장 이주형!"
 
     return strMessage
-
-
 
 def messageMemreturn(sender):
     strMessage = ""
@@ -332,57 +265,9 @@ def messageRemreturn(room):
 
     return strMessage
 
-def messageSeungbeomGraduate():
-    randInt = random.randrange(0, 3)
-    strMessage = ""
-    y, m, d = 2031, 2, 28
-    message_datetime.message_date_calculator(y, m, d)
-    leftdays, lefthours, leftminutes, leftseconds, leftseconds_wa = message_datetime.message_date_calculator(y, m, d)
-
-    if randInt == 0:
-        strMessage = "승범아 대학원 가야지?"
-    elif randInt == 1:
-        strMessage = "승범이가 박사과정을 마치기까지 %d일 %d시간 %d분 %d초 남았습니다."%(leftdays - 1, abs(lefthours), leftminutes, leftseconds)
-    elif randInt == 2:
-        strMessage = "승범이가 박사과정을 마치기까지 " + format(leftseconds_wa, ',') + "초 남았습니다."
-    return strMessage
-
-def messageSupilGraduate():
-    randInt = random.randrange(0, 5)
-    strMessage = ""
-
-    y, m, d = 2024, 11, 27
-    message_datetime.message_date_calculator(y, m, d)
-    leftdays, lefthours, leftminutes, leftseconds, leftseconds_wa = message_datetime.message_date_calculator(y, m, d)
-
-    if randInt == 0:
-        strMessage = "24년은 오지 않습니다..."
-    elif randInt == 1:
-        strMessage = "그런거 물어볼 시간에 일이나 하세요."
-    elif randInt == 2:
-        strMessage = "박수필씨의 소집해제일까지 %d일이 남았습니다."%(leftdays)
-    elif randInt == 3:
-        strMessage = "박수필씨의 소집해제일까지 %d일 %d시간 %d분 %d초 남았습니다."%(leftdays - 1, abs(lefthours), leftminutes, leftseconds)
-    elif randInt == 4:
-        strMessage = "당신이 민간인이 될 때까지 " + format(leftseconds_wa, ',') + "초 남았습니다."
-
-    return strMessage
-
-def messageSeongminGraduate():
-    strMessage = ""
-    randInt = random.randrange(0,2)
-    if randInt == 0: strMessage = "지성민씨가 소집된지 %d일, 소해된지는 %d일이 됐습니다."%((datetime.date.today() - datetime.date(2022,5,22)).days, (datetime.date.today() - datetime.date(2024,2,22)).days)
-    elif randInt == 1: strMessage = "지성민씨의 예비군 소집해제일까지 %d일 남았습니다."%((datetime.date(2031,12,31) - datetime.date.today()).days)
-    return strMessage
-
 def messageSGW():
     messages = ["좀 나가라;;", "뭐하냐;", "좀 꺼라;", "이미 차단당한 유저입니다."]
     return random.choice(messages)
-
-def messageTjoGraduate():
-    strMessage = "zz"
-
-    return strMessage
 
 def messageWabotPower(flag, room):
     strMessage = ""
---
+++ message/message_graduate.py
@@ -0,0 +1,124 @@
+import datetime
+import random
+
+from message import message_datetime
+
+
+def message_graduate(message, room, sender):
+    if "소해" in message or "졸업" in message or "전역" in message:
+        if "병희" in message:
+            return message_bh_graduate()
+        elif "창환" in message:
+            return message_chals_graduate()
+        elif "한수" in message:
+            return message_hansu_graduate()
+        elif "호규" in message:
+            return message_hokyu_graduate()
+        elif "재민" in message:
+            return message_jaemin_graduate()
+        elif "성민" in message:
+            return message_seongmin_graduate()
+        elif "승범" in message:
+            return message_seungbeom_graduate()
+        elif "수필" in message:
+            return message_supil_graduate()
+        elif "태식" in message:
+            return message_tjo_graduate()
+    return None
+
+def message_bh_graduate():
+    strMessage = ""
+
+    randInt = random.randrange(0,2)
+    if randInt == 0: strMessage = "임병희씨가 입대한지 %d일, 전역한지는 %d일이 됐습니다."%((datetime.date.today() - datetime.date(2020,6,30)).days, (datetime.date.today() - datetime.date(2021,12,29)).days)
+    elif randInt == 1: strMessage = "임병희씨의 예비군 소집해제일까지 %d일 남았습니다."%((datetime.date(2029,12,31) - datetime.date.today()).days)
+    return strMessage
+
+def message_chals_graduate():
+    strMessage = "찰스가 입대한지 %d일, 전역한지는 %d일이 됐습니다."%((datetime.date.today() - datetime.date(2020,12,7)).days, (datetime.date.today() - datetime.date(2022,9,1)).days)
+
+    return strMessage
+
+def message_hansu_graduate():
+    strMessage = ""
+    randInt = random.randrange(0,2)
+    if randInt == 0: strMessage = "이한수씨가 소집된지 %d일, 해제된지는 %d일이 됐습니다."%((datetime.date.today() - datetime.date(2022,12,1)).days, (datetime.date.today() - datetime.date(2024,8,31)).days)
+    elif randInt == 1: strMessage = "이한수씨의 민방위 소집해제일까지 %d일 남았습니다."%((datetime.date(2039,1,1) - datetime.date.today()).days)
+
+    return strMessage
+
+def message_hokyu_graduate():
+    strMessage = ""
+    randInt = random.randrange(0, 6)
+
+    if randInt == 0:
+        strMessage = "호규의 부사후 249기 지원을 응원합니다!"
+    elif randInt == 1:
+        strMessage = "호규의 학사 152기 지원을 응원합니다!"
+    elif randInt == 2:
+        strMessage = "호규의 예비군 소집해제일까지 %d일 남았습니다."%((datetime.date(2030,12,31) - datetime.date.today()).days -1)
+    elif randInt == 3:
+        strMessage = "호규의 민방위 소집해제일까지 %d일 남았습니다."%((datetime.date(2041,4,28) - datetime.date.today()).days -1)
+    elif randInt == 4:
+        strMessage = "예비군 1년차는 좀..."
+    elif randInt == 5:
+        strMessage = "하사 김호규의 임기제부사관 만기복무일까지 %d일 남았습니다."%((datetime.date(2027,8,26) - datetime.date.today()).days -1)
+
+    return strMessage
+
+def message_jaemin_graduate():
+    randInt = random.randrange(0, 2)
+    strMessage = ""
+
+    if randInt == 0: strMessage = "재민이가 입대한지 %d일, 전역한지는 %d일이 됐습니다."%((datetime.date.today() - datetime.date(2021,5,9)).days, (datetime.date.today() - datetime.date(2024,3,8)).days)
+    elif randInt == 1: strMessage = "재민이의 예비군 소집해제일까지 %d일 남았습니다."%((datetime.date(2031,12,31) - datetime.date.today()).days)
+
+    return strMessage
+
+def message_seungbeom_graduate():
+    randInt = random.randrange(0, 3)
+    strMessage = ""
+    y, m, d = 2031, 2, 28
+    message_datetime.message_date_calculator(y, m, d)
+    leftdays, lefthours, leftminutes, leftseconds, leftseconds_wa = message_datetime.message_date_calculator(y, m, d)
+
+    if randInt == 0:
+        strMessage = "승범아 대학원 가야지?"
+    elif randInt == 1:
+        strMessage = "승범이가 박사과정을 마치기까지 %d일 %d시간 %d분 %d초 남았습니다."%(leftdays - 1, abs(lefthours), leftminutes, leftseconds)
+    elif randInt == 2:
+        strMessage = "승범이가 박사과정을 마치기까지 " + format(leftseconds_wa, ',') + "초 남았습니다."
+    return strMessage
+
+def message_supil_graduate():
+    randInt = random.randrange(0, 5)
+    strMessage = ""
+
+    y, m, d = 2024, 11, 27
+    message_datetime.message_date_calculator(y, m, d)
+    leftdays, lefthours, leftminutes, leftseconds, leftseconds_wa = message_datetime.message_date_calculator(y, m, d)
+
+    if randInt == 0:
+        strMessage = "24년은 오지 않습니다..."
+    elif randInt == 1:
+        strMessage = "그런거 물어볼 시간에 일이나 하세요."
+    elif randInt == 2:
+        strMessage = "박수필씨의 소집해제일까지 %d일이 남았습니다."%(leftdays)
+    elif randInt == 3:
+        strMessage = "박수필씨의 소집해제일까지 %d일 %d시간 %d분 %d초 남았습니다."%(leftdays - 1, abs(lefthours), leftminutes, leftseconds)
+    elif randInt == 4:
+        strMessage = "당신이 민간인이 될 때까지 " + format(leftseconds_wa, ',') + "초 남았습니다."
+
+    return strMessage
+
+def message_seongmin_graduate():
+    strMessage = ""
+    randInt = random.randrange(0,2)
+    if randInt == 0: strMessage = "지성민씨가 소집된지 %d일, 소해된지는 %d일이 됐습니다."%((datetime.date.today() - datetime.date(2022,5,22)).days, (datetime.date.today() - datetime.date(2024,2,22)).days)
+    elif randInt == 1: strMessage = "지성민씨의 예비군 소집해제일까지 %d일 남았습니다."%((datetime.date(2031,12,31) - datetime.date.today()).days)
+    return strMessage
+
+def message_tjo_graduate():
+    strMessage = "zz"
+
+    return strMessage

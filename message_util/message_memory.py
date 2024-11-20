bfc45cd414e9256d08dbbeb1e50dc23c947b85ae
yymin1022 <yymin1022@gmail.com>
2024-11-20 18:58:06 +0900

    Update message_memory : Refactored Code Style

--- message_util/message_memory.py
+++ message_util/message_memory.py
@@ -4,59 +4,39 @@ import os
 
 def message_memory(message, room, sender):
     if "!기억" in message:
-        return message_remember(message, room)
+        return message_remem(message, room)
     if "뭐였" in message:
-        return messageRemreturn(room)
+        return message_remem_return(room)
     if "뭐더라" in message:
-        return messageMemreturn(sender)
+        return message_mem_return(sender)
 
-def message_remember(message, room):
+def message_remem(message, room):
     message = message.replace("!기억해", "").replace("!기억", "").strip()
 
     if len(message) != 0:
         if os.path.isfile("rem.json"):
-            with open('rem.json', 'r', encoding='utf-8') as f:
+            with open("rem.json", "r", encoding = "utf-8") as f:
                 rem_dict = json.load(f)
         else:
             rem_dict = {}
-
         rem_dict[room] = message
-        json_data = json.dumps(rem_dict, ensure_ascii=False, indent=4)
-
-        with open('rem.json', 'w', encoding='utf-8') as f:
+        json_data = json.dumps(rem_dict, ensure_ascii = False, indent = 4)
+        with open("rem.json", "w", encoding = "utf-8") as f:
             f.write(json_data)
-    else:
-        pass
+    return None
 
-    strMessage = ""
-    return strMessage
-
-def messageMemreturn(sender):
+def message_mem_return(sender):
     if os.path.isfile("mem.json"):
-        with open('mem.json', 'r', encoding='utf-8') as f:
+        with open("mem.json", "r", encoding = "utf-8") as f:
             mem_dict = json.load(f)
-
         if sender in mem_dict:
-            strMessage = mem_dict[sender] + "\\m^^7"
-        else:
-            strMessage = ""
-    else:
-        strMessage = ""
+            return f"{mem_dict[sender]}\\m^^7"
+    return None
 
-    return strMessage
-
-def messageRemreturn(room):
-    strMessage = ""
-
+def message_remem_return(room):
     if os.path.isfile("rem.json"):
-        with open('rem.json', 'r', encoding='utf-8') as f:
+        with open("rem.json", "r", encoding = "utf-8") as f:
             rem_dict = json.load(f)
-
         if room in rem_dict:
-            strMessage = rem_dict[room] + "\\m아마 이거일 듯?"
-        else:
-            strMessage = ""
-    else:
-        strMessage = ""
-
-    return strMessage
+            return f"{rem_dict[room]}\\m아마 이거일 듯?"
+    return None

92af5b973edc61bb1f5a752d7cf824eb66c5e391
yymin1022 <yymin1022@gmail.com>
2024-11-20 20:07:49 +0900

    Update cipher_util : Refactored Code Style

--- util/cipher_util.py
+++ util/cipher_util.py
@@ -3,17 +3,17 @@ from requests.packages.urllib3.util.ssl_ import create_urllib3_context
 
 
 CIPHERS = (
-    'ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:ECDH+HIGH:'
-    'DH+HIGH:ECDH+3DES:DH+3DES:RSA+AESGCM:RSA+AES:RSA+HIGH:RSA+3DES:!aNULL:'
-    '!eNULL:!MD5'
-    ':HIGH:!DH:!aNULL'
+    "ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:ECDH+HIGH:"
+    "DH+HIGH:ECDH+3DES:DH+3DES:RSA+AESGCM:RSA+AES:RSA+HIGH:RSA+3DES:!aNULL:"
+    "!eNULL:!MD5"
+    ":HIGH:!DH:!aNULL"
 )
 
 class DESAdapter(HTTPAdapter):
     def init_poolmanager(self, *args, **kwargs):
-        kwargs['ssl_context'] = create_urllib3_context(ciphers=CIPHERS)
+        kwargs["ssl_context"] = create_urllib3_context(ciphers=CIPHERS)
         return super(DESAdapter, self).init_poolmanager(*args, **kwargs)
 
     def proxy_manager_for(self, *args, **kwargs):
-        kwargs['ssl_context'] = create_urllib3_context(ciphers=CIPHERS)
+        kwargs["ssl_context"] = create_urllib3_context(ciphers=CIPHERS)
         return super(DESAdapter, self).proxy_manager_for(*args, **kwargs)

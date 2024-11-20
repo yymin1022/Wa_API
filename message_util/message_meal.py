819b515cbfb1e99297d909f8679d1bf73ecb8f26
yymin1022 <yymin1022@gmail.com>
2024-11-20 20:00:32 +0900

    Update message_meal : Refactored Code Style

--- message_util/message_meal.py
+++ message_util/message_meal.py
@@ -124,14 +124,14 @@ def message_meal_nsu():
         bokji_data = "boardIdList=467&includeProperties=1&parentBoardContentId=-1&isAvailable=1&isPrivate=0&isAlwaysOnTop=0&isDeleted=0&orderByCode=4"
         cafe_data = "boardIdList=468&includeProperties=1&parentBoardContentId=-1&isAvailable=1&isPrivate=0&isAlwaysOnTop=0&isDeleted=0&orderByCode=4"
 
-        bokji_response = requests.post(meal_url, headers={"Content-Type": "application/x-www-form-urlencoded"}, data=bokji_data).json()
+        bokji_response = requests.post(meal_url, headers = {"Content-Type": "application/x-www-form-urlencoded"}, data = bokji_data).json()
         bokji_response = dict(bokji_response)
         meal_date = bokji_response["body"]["list"][0]["title"]
         bokji_list = bokji_response["body"]["list"][0]["properties"]["food_list"][0]
         for meal_data in bokji_list:
             meal_list_0.append(f"{meal_data[1]}\n")
 
-        cafe_response = requests.post(meal_url, headers={"Content-Type": "application/x-www-form-urlencoded"}, data=cafe_data).json()
+        cafe_response = requests.post(meal_url, headers = {"Content-Type": "application/x-www-form-urlencoded"}, data = cafe_data).json()
         cafe_response = dict(cafe_response)
         cafe0_list = cafe_response["body"]["list"][0]["properties"]["food_list"][0]
         cafe1_list = cafe_response["body"]["list"][0]["properties"]["food_list"][1]

cbceb19f33bbec12cc4c68e79b1c0455ca706fa4
yymin1022 <yymin1022@gmail.com>
2024-11-20 19:37:46 +0900

    Update message_datetime : Added Exception Handling for Timezone Function

--- message_util/message_datetime.py
+++ message_util/message_datetime.py
@@ -80,13 +80,13 @@ def message_timezone(message):
         message = message.replace("!시간 ", "").replace(" ", "")
         if message.startswith("+") or message.startswith("-"):
             if int(message) > 12 or int(message) < -11:
-                raise
+                raise TypeError
             offset = datetime.timedelta(hours=int(message))
         else:
             hours, minutes = map(int, message.split(":"))
             offset = datetime.timedelta(hours=hours, minutes=minutes)
         adjusted_time = datetime.datetime.now(datetime.timezone.utc) + offset
-        str_message = f"현재 UTC{message}의 시간은 ", adjusted_time.strftime("%Y-%m-%d %H:%M:%S") , "입니다."
-    except TypeError:
-        str_message = "사용 형식이 잘못됐거나 존재하지 않는 시간대입니다.\\m사용법: !시간 +9 or !시간 -11"
-    return str_message
+        return f"현재 UTC{message}의 시간은 ", adjusted_time.strftime("%Y-%m-%d %H:%M:%S") , "입니다."
+    except TypeError and ValueError:
+        pass
+    return "사용 형식이 잘못됐거나 존재하지 않는 시간대입니다.\\m사용법: !시간 +9 or !시간 -11"

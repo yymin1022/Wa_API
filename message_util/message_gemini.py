e1fcba907f0b52da5ac480bb69f303538b05c39f
yymin1022 <yymin1022@gmail.com>
2025-01-26 18:01:37 +0900

    Update message_gemini : Changed child gemini model version

--- message_util/message_gemini.py
+++ message_util/message_gemini.py
@@ -9,7 +9,6 @@ dotenv.load_dotenv()
 genai.configure(api_key = os.environ.get("GEMINI_API_KEY"))
 gemini_model_child = genai.GenerativeModel(
     model_name = "gemini-1.5-flash-8b-latest",
-
     system_instruction = "당신은 대한민국의 초등학생입니다. \
                         한국어 반말을 이용해서 대답을 해주면 됩니다. \
                         하지만 답변의 진실 여부는 절대로 틀리면 안됩니다. \

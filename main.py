5568ea75ae2cdeaaf21815f44e138a16d964f283
yymin1022 <yymin1022@gmail.com>
2025-04-13 02:35:47 +0900

    Update main.py : Refactor based on FastAPI, from Flask

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
-from flask import Flask, jsonify, redirect, request
-from flask_cors import CORS
-from waitress import serve
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse, RedirectResponse
 
-import os
 import json
+import os
+import uvicorn
 
 from message import get_wa_reply
 
-flaskApp = Flask(__name__)
-CORS(flaskApp, resources = {r"*": {"origins": "*"}})
+fastApiApp = FastAPI()
  
-@flaskApp.route("/")
+@fastApiApp.get("/")
 def main_page():
-    return redirect("https://github.com/yymin1022/Wa_API", code = 302)
+    return RedirectResponse(url = "https://github.com/yymin1022/Wa_API")
 
-@flaskApp.route("/getMessage", methods = ["POST"])
-def get_message():
+@fastApiApp.post("/getMessage")
+async def get_message(request: Request):
     err_code = 0
     err_message = "RESULT OK"
 
     try:
-        input_data = request.get_json()
+        input_data = await request.json()
         input_message = input_data["msg"]
         input_room = input_data["room"]
         input_sender = input_data["sender"]
@@ -34,7 +33,7 @@ def get_message():
                              dict([("msg", ""),
                                    ("room", ""),
                                    ("sender", "")]))])
-        return jsonify(reply_data)
+        return JSONResponse(content = reply_data)
 
     reply_message = None
     if os.path.isfile("power.json"):
@@ -61,7 +60,7 @@ def get_message():
                         dict([("msg", reply_message),
                               ("room", input_room),
                               ("sender", input_sender)]))])
-    return jsonify(reply_data)
+    return JSONResponse(content = reply_data)
  
 if __name__ == "__main__":
-    serve(flaskApp, host="0.0.0.0", port=80)
+    uvicorn.run("main:fastApiApp", host = "0.0.0.0", port = 80)

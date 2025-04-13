3905e03987c6f2ea9cd60e7ccccf19f275d6ad3d
yymin1022 <yymin1022@gmail.com>
2025-04-13 12:39:17 +0900

    Update main.py : Improved message reply logic

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ from message import get_wa_reply
 fastApiApp = FastAPI()
 fastApiApp.add_middleware(
     CORSMiddleware,
-    allow_credentials=True,
-    allow_headers=["*"],
-    allow_methods=["*"],
-    allow_origins=["*"]
+    allow_credentials = True,
+    allow_headers = ["*"],
+    allow_methods = ["*"],
+    allow_origins = ["*"]
 )
 
 @fastApiApp.get("/")
@@ -23,8 +23,13 @@ def main_page():
 
 @fastApiApp.post("/getMessage")
 async def get_message(request: Request):
-    err_code = 0
-    err_message = "RESULT OK"
+    reply_data = dict([("RESULT",
+                        dict([("RESULT_CODE", 0),
+                                ("RESULT_MSG", "RESULT OK")])),
+                        ("DATA",
+                         dict([("msg", ""),
+                               ("room", ""),
+                               ("sender", "")]))])
 
     try:
         input_data = await request.json()
@@ -32,20 +37,13 @@ async def get_message(request: Request):
         input_room = input_data["room"]
         input_sender = input_data["sender"]
     except Exception as err_data:
-        err_code = 200
-        err_message = repr(err_data)
-        reply_data = dict([("RESULT",
-                            dict([("RESULT_CODE", err_code),
-                                    ("RESULT_MSG", err_message)])),
-                            ("DATA",
-                             dict([("msg", ""),
-                                   ("room", ""),
-                                   ("sender", "")]))])
+        reply_data["RESULT"]["RESULT_CODE"] = 200
+        reply_data["RESULT"]["RESULT_MSG"] = repr(err_data)
         return JSONResponse(content = reply_data)
 
     reply_message = None
     if os.path.isfile("power.json"):
-        with open('power.json', 'r', encoding='utf-8') as f:
+        with open("power.json", "r", encoding = "utf-8") as f:
             power_dict = json.load(f)
             if power_dict.get(input_room) is None:
                 reply_message = get_wa_reply(input_message, input_room, input_sender)
@@ -57,17 +55,16 @@ async def get_message(request: Request):
     else:
         reply_message = get_wa_reply(input_message, input_room, input_sender)
 
-    if reply_message is None:
-        err_code = 100
-        err_message = "None WA Bot Message Found"
+    if reply_message is not None:
+        reply_data["RESULT"]["RESULT_CODE"] = 0
+        reply_data["RESULT"]["RESULT_MSG"] = "RESULT OK"
+        reply_data["DATA"]["msg"] = reply_message
+        reply_data["DATA"]["room"] = input_room
+        reply_data["DATA"]["sender"] = input_sender
+    else:
+        reply_data["RESULT"]["RESULT_CODE"] = 100
+        reply_data["RESULT"]["RESULT_MSG"] = "None WA Bot Message Found"
 
-    reply_data = dict([("RESULT",
-                        dict([("RESULT_CODE", err_code),
-                              ("RESULT_MSG", err_message)])),
-                       ("DATA",
-                        dict([("msg", reply_message),
-                              ("room", input_room),
-                              ("sender", input_sender)]))])
     return JSONResponse(content = reply_data)
  
 if __name__ == "__main__":

89ffe7be6c518daa98907f432e31ebc35fcba0fa
yymin1022 <yymin1022@gmail.com>
2026-05-08 21:26:11 +0900

    Update main.py : Adjust import

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse, RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.concurrency import run_in_threadpool
 
 import uvicorn
 

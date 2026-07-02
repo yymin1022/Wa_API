cbc5423a320b5841469ac0a75a47f0d25668b373
yymin1022 <yymin1022@gmail.com>
2026-07-03 02:01:51 +0900

    Update test_util_direct.py : Use WaMessage model for direct reply tests

--- test_util/test_util_direct.py
+++ test_util/test_util_direct.py
@@ -1,8 +1,14 @@
 from message import get_wa_reply
+from models import WaMessage
 
 
 input_message = input("테스트 할 Message를 입력하세요: ")
-reply_message = get_wa_reply(input_message, "Test_Room1", "Test_Sender1")
+wa_message = WaMessage(
+    msg = input_message,
+    room = "Test_Room1",
+    sender = "Test_Sender1"
+)
+reply_message = get_wa_reply(wa_message)
 
 if reply_message is None:
     reply_message = "None WA Bot Message Found"
